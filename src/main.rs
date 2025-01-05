1981161cbbd965a66ad0a1da8bbec3f8231edab1
matheus de faria <mattheusdefaria@proton.me>
2025-01-05 17:27:35 -0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::vec;
 // Jogo da forca
 // 05/01/2024
 // Autor: Matheus de Faria
-
+#[allow(unused)]
 use std::{io,process};
 
 fn main() {
-    println!("--- Jogo da Forca ---");
+    jogo_forca();
+/*     println!("--- Jogo da Forca ---");
     let mut jogador = String::new();
     let mut opc = String::new();
 
@@ -31,17 +33,35 @@ fn main() {
             println!("--- Fim de Jogo! ---\nAdeus !");
             process::exit(1);
         }
-    };
+    }; */
 
 }
 
 fn jogo_forca() {
     let palavra_secreta = String::from("casa");
+    let nova_palavra = String::new();
     let mut vidas: i32 = 6;
-    let mut letra = String::new();
+    let mut letras_digitadas: Vec<String> = Vec::new();
 
-    println!("Por favor digite uma letra: ");
-    io::stdin()
-    .read_line(&mut letra)
-    .expect("Erro ao ler letra");
+    loop {
+        let mut letra = String::new();
+        println!("Por favor digite uma letra: ");
+        io::stdin()
+        .read_line(&mut letra)
+        .expect("Erro ao ler letra");
+
+        let letra = letra.trim().to_string();
+        if letra.len() > 1 {
+            println!("Entrada Inválida !");
+            continue;
+        };
+
+        if letras_digitadas.contains(&letra) {
+            println!("\n⚠️ Letra já digitada ⚠️\n");
+            continue;
+        }
+
+        letras_digitadas.push(letra.clone());
+
+    }
 }
